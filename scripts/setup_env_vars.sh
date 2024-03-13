671ccff498c232c766e526d8bfeca8038c358cc0
Sajid Ishaq <Sajidishaq007@gmail.com>
2024-03-13 17:33:35 +0500

    Set up AWS CodeDeploy

---
+++ scripts/setup_env_vars.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo 'Install NPM Dependencies: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+
+echo 'Navigate to Project Directory: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+cd /home/ubuntu/thenaturebeautyflowers >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+
+echo 'Install Front-end Dependencies: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log 
+npm ci >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+
+echo 'Navigate to Project's Backend Directory: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+cd /home/ubuntu/thenaturebeautyflowers/backend >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+
+echo 'Install Back-end Dependencies: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log 
+npm ci >> /home/ubuntu/thenaturebeautyflowers/deploy.log
---
+++ scripts/start_server.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo 'Start ExpressJS Server: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+
+echo 'Navigate to Project's Backend Directory: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+cd /home/ubuntu/thenaturebeautyflowers/backend >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+
+echo 'Stop Already Running Servers if any: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+pm2 stop all >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+pm2 delete all >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+
+echo 'Start ExpressJS Backend Server: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+pm2 start index.js -i max --watch >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+
+echo 'Build ReactJS Frontend: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+
+echo 'Navigate to Project Directory: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+cd /home/ubuntu/thenaturebeautyflowers >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+
+echo 'Run a Production Build: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+npm run build >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+
+echo 'Serve ReactJS Frontend: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+
+echo 'Navigate to Project Directory: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+cd /home/ubuntu/thenaturebeautyflowers >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+
+echo 'Run a Production Build: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+serve -s build >> /home/ubuntu/thenaturebeautyflowers/deploy.log

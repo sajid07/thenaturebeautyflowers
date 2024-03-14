570fcb529b3b86e9fd531620d0b7cc228d1b51c7
Sajid Ishaq <Sajidishaq007@gmail.com>
2024-03-14 22:36:16 +0500

    fix syntax error

--- scripts/setup_env_vars.sh
+++ scripts/setup_env_vars.sh
@@ -5,7 +5,7 @@ echo 'Fetch SSM Parameters: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
 SSMParams=$(aws ssm describe-parameters)
 
 echo 'Fetch and Store Values of each SSM Parameter: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
-for param in SSMParams['Parameters'] do
+for param in $SSMParams['Parameters'] do
     ENV_VAR_NAME=$param['Name']
     echo "Getting parameter $ENV_VAR_NAME from SSM parameter store if it exists and setting into the variable $ENV_VAR_NAME"
     SSM_VALUE=`aws ssm get-parameters --with-decryption --names "${$ENV_VAR_NAME}" --query 'Parameters[*].Value' --output text`

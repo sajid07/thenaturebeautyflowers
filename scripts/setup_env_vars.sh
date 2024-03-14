d503c2e484278fe01377b8f0edbe01dc5fbc66c4
Sajid Ishaq <Sajidishaq007@gmail.com>
2024-03-14 23:34:21 +0500

    optimize bash variables & add log statements

--- scripts/setup_env_vars.sh
+++ scripts/setup_env_vars.sh
@@ -2,16 +2,15 @@
 source /home/ubuntu/.bashrc
 
 echo 'Fetch SSM Parameters: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
-SSMParams=$(aws ssm describe-parameters | jq '.Parameters')
+SSMParams=$(aws ssm describe-parameters --query 'Parameters[*].Name')
 
 echo 'Fetch and Store Values of each SSM Parameter: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
-for param in $SSMParams; do
-    ENV_VAR_NAME=$(echo $param | jq '.Name')
-    echo "Getting parameter $ENV_VAR_NAME from SSM parameter store if it exists and setting into the variable $ENV_VAR_NAME"
-    SSM_VALUE=$(aws ssm get-parameters --with-decryption --names "$ENV_VAR_NAME" --query 'Parameters[*].Value' --output text)
-    COMMAND="export $ENV_VAR_NAME=$SSM_VALUE"
+for param in $($SSMParams | jq '.[]'); do
+    echo "Getting parameter $param from SSM parameter store if it exists and setting into the variable $param" >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+    SSM_VALUE=$(aws ssm get-parameters --with-decryption --names "$param" --query 'Parameters[*].Value' --output text)
+    COMMAND="export $param=$SSM_VALUE"
     eval ${COMMAND}
 done
 
 echo 'Store Newly created environment variables to an .env file for production build: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log 
-env | grep -e REACT_APP_ >> /home/ubuntu/thenaturebeautyflowers/.env.production >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+env | grep -e REACT_APP_ >> /home/ubuntu/thenaturebeautyflowers/.env.production

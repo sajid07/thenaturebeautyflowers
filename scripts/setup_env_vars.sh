1d78f495e425c020df9f6469c0599454f88fd9ba
Sajid Ishaq <Sajidishaq007@gmail.com>
2024-03-14 22:34:11 +0500

    fix json key access in bash

--- scripts/setup_env_vars.sh
+++ scripts/setup_env_vars.sh
@@ -5,12 +5,13 @@ echo 'Fetch SSM Parameters: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
 SSMParams=$(aws ssm describe-parameters)
 
 echo 'Fetch and Store Values of each SSM Parameter: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
-for param in ${SSMParams.Parameters} do
-    echo "Getting parameter ${param.Name} from SSM parameter store if it exists and setting into the variable ${param.Name}"
-    SSM_VALUE=`aws ssm get-parameters --with-decryption --names "${param.Name}"  --query 'Parameters[*].Value' --output text`
+for param in SSMParams['Parameters'] do
+    ENV_VAR_NAME=$param['Name']
+    echo "Getting parameter $ENV_VAR_NAME from SSM parameter store if it exists and setting into the variable $ENV_VAR_NAME"
+    SSM_VALUE=`aws ssm get-parameters --with-decryption --names "${$ENV_VAR_NAME}" --query 'Parameters[*].Value' --output text`
     COMMAND="export $ENV_VAR_NAME=$SSM_VALUE"
     eval ${COMMAND}
 done
 
 echo 'Store Newly created environment variables to an .env file for production build: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log 
-env | grep -e REACT_APP_ >> .env.production >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+env | grep -e REACT_APP_ >> /home/ubuntu/thenaturebeautyflowers/.env.production >> /home/ubuntu/thenaturebeautyflowers/deploy.log

85c18811227b252cfad4c23d383dc9e22307242a
Sajid Ishaq <Sajidishaq007@gmail.com>
2024-03-14 20:55:44 +0500

    manually Add nvm path

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 source /home/ubuntu/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 echo 'Start ExpressJS Server: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
 
 echo 'Navigate to Project\'s Backend Directory: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log

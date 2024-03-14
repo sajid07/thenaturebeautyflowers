54e00acbbe37b7b91bf13dc92b368acba6104238
Sajid Ishaq <Sajidishaq007@gmail.com>
2024-03-14 23:08:11 +0500

    try echo in variable

--- scripts/setup_env_vars.sh
+++ scripts/setup_env_vars.sh
@@ -6,7 +6,7 @@ SSMParams=$(aws ssm describe-parameters | jq '.Parameters')
 
 echo 'Fetch and Store Values of each SSM Parameter: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
 for param in $SSMParams do
-    ENV_VAR_NAME=$($param | jq '.Name')
+    ENV_VAR_NAME=$(echo $param | jq '.Name')
     echo "Getting parameter $ENV_VAR_NAME from SSM parameter store if it exists and setting into the variable $ENV_VAR_NAME"
     SSM_VALUE=$(aws ssm get-parameters --with-decryption --names "${$ENV_VAR_NAME}" --query 'Parameters[*].Value' --output text)
     COMMAND="export $ENV_VAR_NAME=$SSM_VALUE"

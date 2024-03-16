70f25390db43f821106202787d5cf35bf820b5aa
Sajid Ishaq <Sajidishaq007@gmail.com>
2024-03-16 17:36:47 +0500

    Integrate AWS CodeBuild with Github Actions for CI/CD

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
-source /home/ubuntu/.bashrc
-export NVM_DIR="/home/ubuntu/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+echo 'Install and configure Utilities to be used by subsequent scripts...'
+apt-get update -y
+apt-get install jq -y
+aws configure set region $AWS_REGION
 
-# Erase Old Deployment logs and Environment Variables
-> /home/ubuntu/thenaturebeautyflowers/deploy.log
-> /home/ubuntu/thenaturebeautyflowers/.env.production
+echo 'Install NPM Dependencies...'
 
-echo 'Navigate to Project Directory: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
-cd /home/ubuntu/thenaturebeautyflowers >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+echo 'Install Front-end Dependencies...' 
+npm cache clean --force && npm i
 
-
-echo 'Install NPM Dependencies: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
-
-echo 'Navigate to Project Directory: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
-cd /home/ubuntu/thenaturebeautyflowers >> /home/ubuntu/thenaturebeautyflowers/deploy.log
-
-echo 'Install Front-end Dependencies: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log 
-npm cache clean --force && npm i >> /home/ubuntu/thenaturebeautyflowers/deploy.log
-
-echo 'Navigate to Project Backend Directory: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
-cd /home/ubuntu/thenaturebeautyflowers/backend >> /home/ubuntu/thenaturebeautyflowers/deploy.log
-
-echo 'Install Back-end Dependencies: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log 
-npm cache clean --force && npm i >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+echo 'Install Back-end Dependencies...' 
+cd backend && npm cache clean --force && npm i
--- scripts/setup_env_vars.sh
+++ scripts/setup_env_vars.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-source /home/ubuntu/.bashrc
-
-echo 'Fetch SSM Parameters: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+echo 'Fetch SSM Parameters...'
 SSMParams=$(aws ssm describe-parameters --query 'Parameters[*].Name' | jq '.[]')
 
-echo 'Fetch and Store Values of each SSM Parameter: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+echo 'Fetch and Store Values of each SSM Parameter...'
 for param in $SSMParams; do
-    echo "Getting parameter $param from SSM parameter store if it exists and setting into the variable $param" >> /home/ubuntu/thenaturebeautyflowers/deploy.log 
+    echo "Getting parameter $param from SSM parameter store if it exists and setting into the variable $param" 
     SSM_CMD="aws ssm get-parameter --with-decryption --name $param --query 'Parameter.Value' --output text"
     SSM_VALUE=$(eval ${SSM_CMD})
     COMMAND="export $param=$SSM_VALUE"
     eval ${COMMAND}
 done
 
-echo 'Store Newly created environment variables to an .env file for production build: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log 
-env | grep -e REACT_APP_ >> /home/ubuntu/thenaturebeautyflowers/.env.production
+echo 'Store Newly created environment variables to an .env file for production build...' 
+env | grep -e REACT_APP_ >> .env.production

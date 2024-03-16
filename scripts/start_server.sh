7aa3b2cf83f42f06968156c766a6ababd4b14246
Sajid Ishaq <Sajidishaq007@gmail.com>
2024-03-16 21:28:44 +0500

    remove unnecessary script

--- scripts/start_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo 'Run ExpressJS Binary Executable: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
-exec backend/thenaturebeautyflowers-api >> /home/ubuntu/thenaturebeautyflowers/deploy.log

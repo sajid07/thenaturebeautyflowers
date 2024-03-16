d421a67ba76ad5181ad6d542560744af220131d6
Sajid Ishaq <Sajidishaq007@gmail.com>
2024-03-16 20:53:57 +0500

    fic deployment issues

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo 'Run ExpressJS Binary Executable: ' >> /home/ubuntu/thenaturebeautyflowers/deploy.log
-exec thenaturebeautyflowers-api >> /home/ubuntu/thenaturebeautyflowers/deploy.log
+exec backend/thenaturebeautyflowers-api >> /home/ubuntu/thenaturebeautyflowers/deploy.log

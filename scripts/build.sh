8c6a6f777669a3457833a218b319e7d738924b9b
Sajid Ishaq <Sajidishaq007@gmail.com>
2024-03-16 20:04:16 +0500

    fix build optimization errors

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 echo 'Build ReactJS Frontend...'
 
-echo 'Navigate to Project Directory...'
-cd /home/ubuntu/thenaturebeautyflowers
-
 echo 'Set Environment variables to optimize ReactJS build...'
 export NODE_OPTIONS=--max-old-space-size=$(expr $(echo "$(free -m)" | awk '/^Mem:/ {print $2}') - 512)
+export IMAGE_INLINE_SIZE_LIMIT=0
 
 echo 'Store Newly created environment variables to an .env file for production build...' 
-env | grep -e NODE_OPTIONS >> /home/ubuntu/thenaturebeautyflowers/.env.production
+env | grep -e NODE_OPTIONS -e IMAGE_INLINE_SIZE_LIMIT >> .env.production
 
 echo 'Run a Production Build...'
 npm run build
 
 
-echo 'Build and Bundle ExpressJS Backend...'
+echo 'Build and Package ExpressJS Backend...'
 
 echo 'Navigate to Project Backend Directory...'
 cd backend
 
-echo 'Output CommonJS bundle...'
-npx esbuild index.js  --bundle --outfile=build.cjs --format=cjs --platform=node
-
-echo 'Generate binary executable file from previously generated bundle...'
-npx pkg build.cjs --options $NODE_OPTIONS --output thenaturebeautyflowers-api
+echo 'Generate binary executable file from ExpressJS Backend Application...'
+npx pkg index.js --options $NODE_OPTIONS --output thenaturebeautyflowers-api

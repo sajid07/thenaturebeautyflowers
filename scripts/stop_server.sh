8edaec7bff854e024ae37007e3b41f0ea5eba744
Sajid Ishaq <Sajidishaq007@gmail.com>
2024-03-17 22:49:07 +0500

    ignore pm2 warning

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -9,5 +9,5 @@ echo 'Remove old Application Build artifacts...'
 rm -rf $HOME_DIR/thenaturebeautyflowers/{*,.*}
 
 echo 'Stop Running ExpressJS Backend Server Applications if any...'
-pm2 stop all
-pm2 delete all
+pm2 stop --silent all
+pm2 delete --silent all

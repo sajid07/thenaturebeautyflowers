0027d6e743ae567a2572fcbb4a2957dac751099a
Sajid Ishaq <Sajidishaq007@gmail.com>
2024-03-18 00:49:52 +0500

    Fix scripts to redirect all output

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,10 +2,9 @@
 HOME_DIR=/home/ubuntu
 LOG_FILE=$HOME_DIR/deploy.log
 SOURCE_DIR=$HOME_DIR/thenaturebeautyflowers
-source $HOME_DIR/.bashrc
 export NVM_DIR="$HOME_DIR/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" >> $LOG_FILE  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" >> $LOG_FILE  # This loads nvm bash_completion
 
 echo 'Start and Keep Running ExpressJS Backend Server Application in the Background...' >> $LOG_FILE
 cd $SOURCE_DIR/backend >> $LOG_FILE

a5d416dd540d03250306c451eb72a62f354099c7
Sajid Ishaq <Sajidishaq007@gmail.com>
2024-03-17 23:07:37 +0500

    Redirect std output of deploy scripts

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 HOME_DIR=/home/ubuntu
+SOURCE_DIR=$HOME_DIR/thenaturebeautyflowers
 source $HOME_DIR/.bashrc
 export NVM_DIR="$HOME_DIR/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-echo 'Start and Keep Running ExpressJS Backend Server Application in the Background...'
-cd backend
-pm2 start thenaturebeautyflowers-api -i max --watch
+echo 'Start and Keep Running ExpressJS Backend Server Application in the Background...' >> $SOURCE_DIR/deploy.log
+cd backend >> $SOURCE_DIR/deploy.log
+pm2 start thenaturebeautyflowers-api -i max --watch >> $SOURCE_DIR/deploy.log
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 HOME_DIR=/home/ubuntu
+SOURCE_DIR=$HOME_DIR/thenaturebeautyflowers
 source $HOME_DIR/.bashrc
 export NVM_DIR="$HOME_DIR/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-echo 'Remove old Application Build artifacts...'
-rm -rf $HOME_DIR/thenaturebeautyflowers/{*,.*}
+echo 'Remove old Application Build artifacts...' >> $SOURCE_DIR/deploy.log
+rm -rf $SOURCE_DIR/{*,.*}
 
 echo 'Stop Running ExpressJS Backend Server Applications if any...'
-pm2 stop --silent all
-pm2 delete --silent all
+pm2 stop --silent all >> $SOURCE_DIR/deploy.log
+pm2 delete --silent all >> $SOURCE_DIR/deploy.log

7dfacb28d8e734c5a6ff2a595ab689009577766d
Sajid Ishaq <Sajidishaq007@gmail.com>
2024-03-17 23:29:03 +0500

    change logs directory

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 HOME_DIR=/home/ubuntu
+LOG_FILE=$HOME_DIR/deploy.log
 SOURCE_DIR=$HOME_DIR/thenaturebeautyflowers
 source $HOME_DIR/.bashrc
 export NVM_DIR="$HOME_DIR/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-echo 'Remove old Application Build artifacts...' >> $SOURCE_DIR/deploy.log
+echo 'Remove old Application Build artifacts...' >> $LOG_FILE
 rm -rf $SOURCE_DIR/{*,.*}
 
-echo 'Stop Running ExpressJS Backend Server Applications if any...'
-pm2 stop --silent all >> $SOURCE_DIR/deploy.log
-pm2 delete --silent all >> $SOURCE_DIR/deploy.log
+echo 'Stop Running ExpressJS Backend Server Applications if any...' >> $LOG_FILE
+pm2 stop --silent all >> $LOG_FILE
+pm2 delete --silent all >> $LOG_FILE

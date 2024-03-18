1b2c1996adf2194cbf77d6e021b15d980bb22361
Sajid Ishaq <Sajidishaq007@gmail.com>
2024-03-18 15:15:34 +0500

    try different method of output redirection

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+# Redirect all output to log file
+exec 1>>/opt/thenaturebeautyflowers-deploy.log
+exec 2>&1
+
 HOME_DIR=/home/ubuntu
-LOG_FILE=$HOME_DIR/deploy.log
 SOURCE_DIR=$HOME_DIR/thenaturebeautyflowers
 export NVM_DIR="$HOME_DIR/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" >> $LOG_FILE  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" >> $LOG_FILE  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-echo 'Start and Keep Running ExpressJS Backend Server Application in the Background...' >> $LOG_FILE
-cd $SOURCE_DIR/backend >> $LOG_FILE
-pm2 start thenaturebeautyflowers-api -i max --watch >> $LOG_FILE
+echo 'Start and Keep Running ExpressJS Backend Server Application in the Background...'
+cd $SOURCE_DIR/backend
+pm2 start thenaturebeautyflowers-api -i max --watch --log /opt/thenaturebeautyflowers-api.log
+pm2 save # Respawn after reboot
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+# Redirect all output to log file
+exec 1>>/opt/thenaturebeautyflowers-deploy.log
+exec 2>&1
+
 HOME_DIR=/home/ubuntu
-LOG_FILE=$HOME_DIR/deploy.log
 SOURCE_DIR=$HOME_DIR/thenaturebeautyflowers
 export NVM_DIR="$HOME_DIR/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" >> $LOG_FILE  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" >> $LOG_FILE  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-echo 'Remove old Application Build artifacts...' >> $LOG_FILE
-find $SOURCE_DIR -mindepth 1 -delete >> $LOG_FILE
+echo 'Remove old Application Build artifacts...'
+find $SOURCE_DIR -mindepth 1 -delete
 
-echo 'Stop Running ExpressJS Backend Server Applications if any...' >> $LOG_FILE
-pm2 stop --silent all >> $LOG_FILE
-pm2 delete --silent all >> $LOG_FILE
+echo 'Stop Running ExpressJS Backend Server Applications and remove logs if any...'
+pm2 stop --silent all
+pm2 delete --silent all
+pm2 flush

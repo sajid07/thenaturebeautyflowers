dd91787933b92a2298411a3352d21851442f3e7a
Sajid Ishaq <Sajidishaq007@gmail.com>
2024-03-18 16:51:08 +0500

    Change build order

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+echo 'Build and Package ExpressJS Backend...'
+
+echo 'Navigate to Project Backend Directory...'
+cd backend
+
+echo 'Output CommonJS bundle...'
+npm run build
+
+echo 'Generate binary executable file from ExpressJS Backend Application...'
+npx @yao-pkg/pkg --output thenaturebeautyflowers-api build.cjs
+
+
 echo 'Build ReactJS Frontend...'
 
 echo 'Set Environment variables to optimize ReactJS build...'
@@ -10,15 +22,3 @@ env | grep -e NODE_OPTIONS -e IMAGE_INLINE_SIZE_LIMIT >> .env.production
 
 echo 'Run a Production Build...'
 npm run build
-
-
-echo 'Build and Package ExpressJS Backend...'
-
-echo 'Navigate to Project Backend Directory...'
-cd backend
-
-echo 'Output CommonJS bundle...'
-npm run build
-
-echo 'Generate binary executable file from ExpressJS Backend Application...'
-npx @yao-pkg/pkg --output thenaturebeautyflowers-api build.cjs

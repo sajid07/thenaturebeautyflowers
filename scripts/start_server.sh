d85cc0dab23f58c7d93e832bc73d54f130a0e975
Sajid Ishaq <Sajidishaq007@gmail.com>
2024-03-21 19:20:55 +0500

    change path for log files

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-source ~/.profile
 
 # Redirect all output to log file
-sudo exec 1>>/opt/thenaturebeautyflowers-deploy.log
-sudo exec 2>&1
+exec 1>>~/thenaturebeautyflowers-deploy.log
+exec 2>&1
 
 SOURCE_DIR=~/thenaturebeautyflowers
-export NVM_DIR="~/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 echo 'Start and Keep Running ExpressJS Backend Server Application in the Background...'
 cd $SOURCE_DIR/backend
-pm2 start thenaturebeautyflowers-api -i max --watch --log /opt/thenaturebeautyflowers-api.log
+pm2 start thenaturebeautyflowers-api -i max --watch --log ~/thenaturebeautyflowers-api.log
 pm2 save # Respawn after reboot
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-source ~/.profile
 
 # Redirect all output to log file
-sudo exec 1>>/opt/thenaturebeautyflowers-deploy.log
-sudo exec 2>&1
+exec 1>>~/thenaturebeautyflowers-deploy.log
+exec 2>&1
 
 SOURCE_DIR=~/thenaturebeautyflowers
-export NVM_DIR="~/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 echo 'Remove old Application Build artifacts...'
 sudo find $SOURCE_DIR -mindepth 1 -delete

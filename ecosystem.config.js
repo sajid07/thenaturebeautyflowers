48ce95a1e33af62808b02c08bd12ebf5b27ba86e
Ahmed Rehan <ar27111994@gmail.com>
2025-08-17 01:12:58 +0500

    Update ecosystem.config.js to Fix App path and arguments parsing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,29 +1,29 @@
 module.exports = {
-  apps : [
-      {
-        "name": "thenaturebeautyflowers-api",
-        "script": "backend/thenaturebeautyflowers-api",
-        "instances": "max",
-        "exec_mode": "cluster",
-        "watch": true,
-        "merge_logs": true,
-        "log_file": "../thenaturebeautyflowers-api.log",
-        "time": true,
-        "env_production": {
-          "NODE_ENV": "production"
-        }
-      },
-      {
-        "name": "thenaturebeautyflowers-ui",
-        "script": "npx",
-        "args": "serve /home/ubuntu/thenaturebeautyflowers/build -s -l 3000 -H 0.0.0.0", // Serve the build folder, -s for single-page application fallback
-        "watch": false,
-        "merge_logs": true,
-        "time": true,
-        "log_file": "../thenaturebeautyflowers.log",
-        "env_production": {
-          "NODE_ENV": "production"
-        }
+  apps: [
+    {
+      "name": "thenaturebeautyflowers-api",
+      "script": "/home/ubuntu/thenaturebeautyflowers/backend/thenaturebeautyflowers-api",
+      "instances": "max",
+      "exec_mode": "cluster",
+      "watch": true,
+      "merge_logs": true,
+      "log_file": "../thenaturebeautyflowers-api.log",
+      "time": true,
+      "env_production": {
+        "NODE_ENV": "production"
+      }
+    },
+    {
+      "name": "thenaturebeautyflowers-ui",
+      "script": "serve",
+      "args": ["/home/ubuntu/thenaturebeautyflowers/build", "-s", "-p", "3000", "--host", "0.0.0.0"],
+      "watch": false,
+      "merge_logs": true,
+      "time": true,
+      "log_file": "../thenaturebeautyflowers.log",
+      "env_production": {
+        "NODE_ENV": "production"
+      }
     }
   ]
 }

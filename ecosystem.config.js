650136ca63924b6e3c7fc81f88e42af22d9543b0
Ahmed Rehan <ar27111994@gmail.com>
2025-08-16 17:20:50 +0500

    Update ecosystem.config.js to include Backend API PM2 Configuration as well

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,26 @@
 module.exports = {
   apps : [
       {
+          "name": "thenaturebeautyflowers-api",
+          "script": "backend/thenaturebeautyflowers-api",
+          "instances": "max",
+          "exec_mode": "cluster",
+          "watch": true,
+          "merge_logs": true,
+          "log_file": "../thenaturebeautyflowers-api.log",
+          "time": true,
+          "env": {
+            "NODE_ENV": "production"
+          }
+      }
+      {
         "name": "thenaturebeautyflowers-ui",
         "script": "npx serve",
         "args": "build -s", // Serve the build folder, -s for single-page application fallback
+        "watch": true,
+        "merge_logs": true,
+        "time": true,
+        "log_file": "../thenaturebeautyflowers.log",
         "env": {
           "NODE_ENV": "production"
         }

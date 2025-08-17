031ed3b42207b9b4f985c648b4675bd61f392edf
Ahmed Rehan <ar27111994@gmail.com>
2025-08-17 14:22:00 +0500

    Update ecosystem.config.js to properly serve Backend API Package

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,10 +5,11 @@ module.exports = {
       "script": "/home/ubuntu/thenaturebeautyflowers/backend/thenaturebeautyflowers-api",
       "instances": "max",
       "exec_mode": "cluster",
-      "watch": true,
+      "watch": false, // Binary files don't need file watching
       "merge_logs": true,
       "log_file": "../thenaturebeautyflowers-api.log",
       "time": true,
+      "interpreter": "none", // This tells PM2 to execute the binary directly
       "env_production": {
         "NODE_ENV": "production"
       }
@@ -16,7 +17,7 @@ module.exports = {
     {
       "name": "thenaturebeautyflowers-ui",
       "script": "npx",
-      "args": ["serve", "/home/ubuntu/thenaturebeautyflowers/build", "-s", "-l 3000", "-n", "-u", "-S"],
+      "args": ["serve", "/home/ubuntu/thenaturebeautyflowers/build", "-s", "-l", "3000", "-n", "-u", "-S"],
       "watch": false,
       "merge_logs": true,
       "time": true,
